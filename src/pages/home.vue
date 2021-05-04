8a81fbe27c1bd45b90e6e9d6e99a5d3009d6ffcb
Fatlum (Lumi) Ibishi <fatlum.ibishi@gmail.com>
2021-05-04 10:25:14 +0200

    UIKit.js update, cors edit

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -603,13 +603,11 @@
               <div class="uk-margin uk-text-center">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4699.828306657711!2d-81.72726112300585!3d25.981852533741353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88daef8596ccb7d7%3A0x9ffb99c7acee95da!2s387%20Capri%20Blvd%2C%20Naples%2C%20FL%2034113%2C%20USA!5e0!3m2!1sen!2sse!4v1604660147120!5m2!1sen!2sse"
-                  width="600"
-                  height="450"
                   frameborder="0"
                   style="border: 0"
-                  allowfullscreen=""
                   aria-hidden="false"
                   tabindex="0"
+                  class="gmaps"
                 ></iframe>
               </div>
             </div>
@@ -803,7 +801,7 @@ export default {
         );
     },
     fetchUsername: async function(username) {
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      const proxyUrl = "";
       return await fetch(
         `${proxyUrl}https://www.instagram.com/${username}/?__a=1`
       ).then((r) => r.json());
